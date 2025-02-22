6f135b09b5032a1aebb0f62e54a6d2b4d75f866c
crea-tivecoder <nyawirawaweru.com>
2025-02-22 23:39:05 +0300

    adds homepage icon

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 // Import the university image directly
-import universityImage from "../assets/universityPic.jpg";
+import universityImage from "../assets/universityPic2.jpg";
 
 function HomePage() {
 
